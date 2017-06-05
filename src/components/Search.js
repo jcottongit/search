f915ed185da6c146c2ff53d80424d6b1e3ec3e7d
Johnny <jcotton02@gmail.com>
2017-06-05 19:28:20 +0100

    Searchbox & FourSquare search string set up

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import { singleQS } from '../utils/helpers';
+import { displayResults } from './Results';
+
+const fourSquareID = 'W4OUNOB0FXBX4TIO5IFY14PE3N4PH5YF5SQ124ROHZ4LVS52';
+const fourSquareSecret = 'K2WIMXBOR2CUNO0Y3ZBNRTKAWZODWQ4NDBCTSTNJNP0RJG4W';
+
+const searchInput = singleQS('#search');
+const searchBox = singleQS('#searchBox');
+
+function searchResults(e) {
+  e.preventDefault();
+  const searchLocation = searchBox.value;
+  const searchValue = searchLocation.replace(' ', '+');
+  const searchString = `https://api.foursquare.com/v2/venues/explore?client_id=${fourSquareID}&client_secret=${fourSquareSecret}&v=20130815&near=${searchValue}&limit=100`;
+
+}
+
+searchInput.addEventListener('submit', searchResults);
