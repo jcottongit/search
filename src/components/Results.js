4bbe718be5442b274af8463eb2186ffe9bcf374d
Johnny <jcotton02@gmail.com>
2017-06-05 20:20:53 +0100

    Exit out of displaying results if empty object passed

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,6 +4,7 @@ import { renderVenue } from './Venue';
 const searchResultsBox = singleQS('#searchResultsBox');
 
 export function displayResults(venues) {
+  if (Object.keys(venues).length < 1) return; // Check if empty object
   const results = venues.groups[0].items;
   const resultsHTML = results.map(result => renderVenue(result)).join('');
 
